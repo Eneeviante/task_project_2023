1cc8620ed825ba308b0065a9d060e31a73c3c670
Максим Цикунов <czikunov.maks@mail.ru>
2023-09-16 12:10:35 +0300

    WordSearch done

--- Assets/App/Scripts/Scenes/SceneWordSearch/Features/Level/BuilderLevelModel/FactoryLevelModel.cs
+++ Assets/App/Scripts/Scenes/SceneWordSearch/Features/Level/BuilderLevelModel/FactoryLevelModel.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using App.Scripts.Libs.Factory;
 using App.Scripts.Scenes.SceneWordSearch.Features.Level.Models.Level;
+using Unity.VisualScripting;
 
 namespace App.Scripts.Scenes.SceneWordSearch.Features.Level.BuilderLevelModel
 {
@@ -22,7 +23,37 @@ namespace App.Scripts.Scenes.SceneWordSearch.Features.Level.BuilderLevelModel
         private List<char> BuildListChars(List<string> words)
         {
             //напиши реализацию не меняя сигнатуру функции
-            throw new NotImplementedException();
+
+            List<char> listChars = new List<char>();
+            Dictionary<char, int> numberChars = new Dictionary<char, int>();
+            foreach(string word in words)
+            {
+                Dictionary<char, int> wordChars = new Dictionary<char, int>();
+                for (int i = 0; i < word.Length; i++)
+                {
+                    if(wordChars.ContainsKey(word[i])) wordChars[word[i]]++;
+                    else wordChars.Add(word[i], 1);
+                }
+
+                foreach(var char_num in wordChars)
+                {
+                    if (!numberChars.ContainsKey(char_num.Key))
+                    {
+                        numberChars.Add(char_num.Key, char_num.Value);
+                        continue;
+                    }
+
+                    if (numberChars[char_num.Key] < char_num.Value)
+                        numberChars[char_num.Key] = char_num.Value;
+
+                }
+            }
+
+            foreach(var pair in numberChars)
+                for (int i = 0; i < pair.Value; i++)
+                    listChars.Add(pair.Key);
+            
+            return listChars;
         }
     }
 }
--- Assets/App/Scripts/Scenes/SceneWordSearch/Features/Level/BuilderLevelModel/ProviderWordLevel/ProviderWordLevel.cs
+++ Assets/App/Scripts/Scenes/SceneWordSearch/Features/Level/BuilderLevelModel/ProviderWordLevel/ProviderWordLevel.cs
@@ -1,5 +1,6 @@
-using System;
 using App.Scripts.Scenes.SceneWordSearch.Features.Level.Models.Level;
+using Newtonsoft.Json;
+using UnityEngine;
 
 namespace App.Scripts.Scenes.SceneWordSearch.Features.Level.BuilderLevelModel.ProviderWordLevel
 {
@@ -8,7 +9,12 @@ namespace App.Scripts.Scenes.SceneWordSearch.Features.Level.BuilderLevelModel.Pr
         public LevelInfo LoadLevelData(int levelIndex)
         {
             //напиши реализацию не меняя сигнатуру функции
-            throw new NotImplementedException();
+
+            TextAsset levelsTextAsset = Resources.Load<TextAsset>("WordSearch/Levels/" + levelIndex.ToString());
+            string jsonlevelContent = levelsTextAsset.text.Replace("\n", "").Replace("\t", "").Replace(" ", "");
+
+            LevelInfo lvl = JsonConvert.DeserializeObject<LevelInfo>(jsonlevelContent);
+            return lvl;           
         }
     }
 }
